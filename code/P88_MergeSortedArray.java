8dbfb3cf8866c069c362813ef6edc3ecbc43c35e
Mereder <550199493@qq.com>
2019-05-06 23:10:33 +0800

    合并排序数组

---
+++ code/P88_MergeSortedArray.java
@@ -0,0 +1,29 @@
+/*
+ * Date :  2019.
+ * Author : Mereder
+ */
+
+package code;
+
+public class P88_MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // 从后向前的写法
+        int i = m-1;
+        int j = n-1;
+        int tail = m+n-1;
+        while(i >= 0 && j >= 0 ){
+            if(nums1[i] > nums2[j]){
+                nums1[tail] = nums1[i];
+                tail--;
+                i--;
+            }
+            else{
+                nums1[tail--] = nums2[j--];
+            }
+        }
+        while(j >= 0){
+            nums1[tail--] = nums2[j--];
+
+        }
+    }
+}
